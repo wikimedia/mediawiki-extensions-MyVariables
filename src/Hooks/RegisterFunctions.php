4bacbde69a342b9823ed75724d5a644d97e3836a
Brian Wolff <bawolff+wn@gmail.com>
2023-01-30 22:09:59 -0800

    Change method of disabling parser cache for {{CURRENTLOGGEDUSER}}
    The goal is to disable parser cache but not CDN cache. The previous
    solution used an article view hook. This was unideal because it
    would often cause MediaWiki to parse the page twice, doubling the
    load time, since it had to parse the page to see if the flag to
    disable cache was present, and then parse the page later to show
    it to the user. This would always happen during a cache reject, but
    it seemed to also happen during ordinary cache misses sometimes, for
    unclear reasons.
    
    Instead we split the cache between logged in and logged out, but
    always mark pages as uncacheable if the user is logged in. This
    means that logged out users can still enjoy parser cache, and CDN
    cache is still enabled.
    
    Some other minor benefits are:
    * The cache report HTML comment will now be correct, reducing confusion
    * This will no longer save things in parser cache that cannot be used,
    potentially leaving more room for important stuff.
    * If you access a page not via the main view (e.g. via api) where the
    old hook doesn't run, you could have gotten an incorrect page view.
    
    [WIK-791]
    
    Change-Id: I7b3d76df3d36a38adf9e4160f69d063d7167dc8e
    (cherry picked from commit 9a1b5004fb5c7fc0328f77630527a13810bff8c3)

--- src/Hooks/RegisterFunctions.php
+++ src/Hooks/RegisterFunctions.php
@@ -4,8 +4,10 @@ namespace MediaWiki\Extension\MyVariables\Hooks;
 
 use MediaWiki\Extension\MyVariables\MyVariables;
 use MediaWiki\Hook\ParserFirstCallInitHook;
+use MediaWiki\Hook\ParserOptionsRegisterHook;
+use ParserOptions;
 
-class RegisterFunctions implements ParserFirstCallInitHook {
+class RegisterFunctions implements ParserFirstCallInitHook, ParserOptionsRegisterHook {
 
 	/**
 	 * Register parser functions associated with MAG_NIFTYVAR as a variable
@@ -15,4 +17,24 @@ class RegisterFunctions implements ParserFirstCallInitHook {
 		$parser->setFunctionHook( 'MAG_REALNAME', [ MyVariables::class, 'getRealName' ], SFH_NO_HASH );
 	}
 
+	/**
+	 * Allow varying cache key for whether user is logged in.
+	 * @inheritDoc
+	 */
+	public function onParserOptionsRegister( &$defaults, &$inCacheKey, &$lazyLoad ) {
+		// We use null here to say use lazy initialization
+		// Since the lazy callback has access to the correct user but we do not.
+		$defaults['MyVarLoggedIn'] = null;
+		$inCacheKey['MyVarLoggedIn'] = true;
+		$lazyLoad['MyVarLoggedIn'] = static function ( ParserOptions $options ) {
+			if ( method_exists( $options, 'getUser' ) ) {
+				// pre 1.37
+				// @phan-suppress-next-line PhanUndeclaredMethod
+				$user = $options->getUser();
+			} else {
+				$user = $options->getUserIdentity();
+			}
+			return $user->isRegistered() ? "yes" : "no";
+		};
+	}
 }
